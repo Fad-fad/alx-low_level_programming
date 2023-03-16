c6997059d0d856f2c29982406b369e9d8773293f
Fad-fad <faduteimi@gmail.com>
2023-03-16 14:11:05 -0700

    Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -HHH
+ * Return 0: Successs
+ */
+int main()
+{fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);}
