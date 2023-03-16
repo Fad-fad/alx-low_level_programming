c3533241e783a3e84c72532be75ce0f3c8232616
Fad-fad <faduteimi@gmail.com>
2023-03-16 14:17:27 -0700

    Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -HHH
- * Return 0: Successs
+ * main - HHH
+ * Return: 0 (Success)
  */
-int main()
-{fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);}
+int main(void)
+{fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1); }
