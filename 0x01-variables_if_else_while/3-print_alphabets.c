16147fe23602599e050f60740787297255da3fae
Fad-fad <faduteimi@gmail.com>
2023-03-17 08:44:39 -0700

    Task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Function of hhsdhj
+ * Return: 0 (Success)
+ */
+int main(void)
+{int i;
+for (i = 97; i <= 122; i++)
+{putchar(i); }
+for (i = 65; i <= 90; i++)
+{putchar(i); }
+putchar('\n');
+return (0); }
