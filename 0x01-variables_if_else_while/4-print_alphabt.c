504751cac0a7ef86a8a6436b2be19da20515aa0d
Fad-fad <faduteimi@gmail.com>
2023-03-17 15:18:55 -0700

    Task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Function of hhsdhj
+ * Return: 0 (Success)
+ */
+int main(void)
+{int i;
+for (i = 97; i <= 122; i++)
+{if (i = 113 || i = 101)
+continue;
+putchar(i); }
+putchar('\n');
+return (0); }
