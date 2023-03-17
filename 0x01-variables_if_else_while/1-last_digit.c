e336a062b2e14420df74c48bd5b326d9d234a575
Fad-fad <faduteimi@gmail.com>
2023-03-17 08:00:36 -0700

    Task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that prints the sign of integers
+ * Return: Alwiays 0 (Success)
+ */
+int main(void)
+{int n;
+int u;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+u = n % 10;
+if (u > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+if (u < 6  && u != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n % 10);
+if (u == 0)
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
+return (0); }
