85909768c4a42054d9ca543c7e4464620ce80778
Fad-fad <faduteimi@gmail.com>
2023-03-17 18:21:12 -0700

    Task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdbool.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int i, j, k, l;
+int i, j, z, h;
 bool is_first_iteration = true;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-for (k = i; k < 10; k++)
+for (z = i; z < 10; z++)
 {
-for (l = 0; l < 10; l++)
+for (h = 0; h < 10; h++)
 {
-if (i == k && l <= j)
+if (i == z && h <= j)
+{
 continue;
+}
 if (is_first_iteration == false)
 {
 putchar(',');
-putchar(' ');
-putchar(i + '0'); }
+putchar(' '); }
+putchar(i + '0');
 putchar(j + '0');
 putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
+putchar(z + '0');
+putchar(h + '0');
 is_first_iteration = false; }}}}
 putchar('\n');
 return (0); }
