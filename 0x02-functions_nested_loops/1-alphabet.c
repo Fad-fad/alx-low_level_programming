29b89e5c0768fd0fe9bff24350b7287ae68ddc5f
Fad-fad <faduteimi@gmail.com>
2023-03-21 12:33:15 -0700

    hi

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  * Return: Always 0
  */
 
 void print_alphabet(void)
-{char c;
+{char c;i
 for (c = 'a'; c <= 'z'; c++)
 { _putchar(c); }
 _putchar('\n');
