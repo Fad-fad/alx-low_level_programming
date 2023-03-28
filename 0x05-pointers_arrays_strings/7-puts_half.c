a4c982ea4e38dcdb501add6717c68c4c6c3b8d6f
Fad-fad <faduteimi@gmail.com>
2023-03-28 10:53:56 -0700

    task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,13 +14,13 @@ j++)
 {}
 if (j % 2 == 0)
 {
-for (i = j / 2 - 1; i < j;
+for (i = j / 2; i < j;
 i++)
 _putchar(str[i]);
 }
 else
 {
-for (i = (j - 1) / 2 - 1; i < j;
+for (i = (j - 1) / 2 + 1; i < j;
 i++)
 _putchar(str[i]);
 }
