9a944b291416fc889ca7c0596d3375ad32211785
Fad-fad <faduteimi@gmail.com>
2023-03-28 10:15:10 -0700

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+/**
+ * puts_half-check the cod
+ * @str: var in 98
+ *
+ * Return: Always 0.
+ */
+#include "main.h"
+void puts_half(char *str)
+{
+int i;
+int j;
+for (j = 0; str[j] != '\0';
+j++)
+{}
+if (j % 2 == 0)
+{for (i = j / 2; i < j;
+i++)
+_putchar(str[i]);
+}
+else
+{for (i = (j - 1) / 2; i < j;
+i++)
+_putchar(str[i]);
+}
+_putchar('\n');
+}
