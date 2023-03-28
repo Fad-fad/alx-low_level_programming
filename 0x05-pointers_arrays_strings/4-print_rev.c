474cdcf51acf39d56e1a8cbf7ad69ec27c99e5ba
Fad-fad <faduteimi@gmail.com>
2023-03-27 17:37:47 -0700

    task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,22 @@
+#include "main.h"
 /**
  * print_rev-check the code
  * @s: var to print
  *
- * Return: Always 0.
+ * Return: Always 0
  */
-#include "main.h"
 void print_rev(char *s)
 {
 int i;
 int j;
 for (i = 0; s[i] != '\0';
 i++)
-;
+{
+}
 for (j = i - 1; j >= 0;
 j--)
-_putchar(s[i]);
+{
+_putchar(s[j]);
+}
 _putchar('\n');
 }
