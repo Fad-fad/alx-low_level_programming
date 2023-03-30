6a56e255582348c548ea98561fcc9852585bfb87
Fad-fad <faduteimi@gmail.com>
2023-03-30 12:52:43 -0700

    task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strncpy- converts a string to an integer
+ * @dest: string to be converted
+ * @src: jfjffkkfk
+ * @n: intg
+ * Return: the int converted from the string
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+int i;
+int j;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (j = 0; src[j] != '\0' && j < n; j++)
+{
+dest[i] = src[j];
+i++;
+}
+return (dest);
+}
