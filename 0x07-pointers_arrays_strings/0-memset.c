0fe94bc1368ef2be9d9629ae0e1e9f7c2fac1932
Fad-fad <faduteimi@gmail.com>
2023-04-03 07:55:18 -0700

    task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_memset- converts a string to an integer
+ * @s: string to be converted
+ * @b: jfjffkkfk
+ * @n: intg
+ * Return: the int converted from the string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
