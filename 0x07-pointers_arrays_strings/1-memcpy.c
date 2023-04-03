605ca35f39bc496c6dd5c9ac73f3e056d207c060
Fad-fad <faduteimi@gmail.com>
2023-04-03 09:32:42 -0700

    task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memcpy- converts a string to an integer
+ * @src: string to be converted
+ * @dest: jfjffkkfk
+ * @n: intg
+ * Return: the int converted from the string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(dest + i) = *(src + i);
+}
+return (dest);
+}
+
