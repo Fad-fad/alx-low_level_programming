0283f380253e69084eea228a472f21b75a262cc2
Fad-fad <faduteimi@gmail.com>
2023-04-05 09:00:05 -0700

    task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - a function that prints the sum of the two diagonals
+ * factorial - a function that prints the sum of the two diagonals
  * of a square matrix of integers
- * @s: 2D array
+ * @n: 2D array
  * Return: nothing
  */
 
@@ -12,7 +12,7 @@ int factorial(int n)
 if (n < 0)
 return (-1);
 else if (n == 0 || n == 1)
-return (1)
+return (1);
 else
 return (n * factorial(n - 1));
 }
