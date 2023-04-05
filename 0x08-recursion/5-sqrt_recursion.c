7521ba1fb53403fdb3a3183a06a873cd051cb853
Fad-fad <faduteimi@gmail.com>
2023-04-05 16:00:53 -0700

    task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,29 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - a function that prints the sum of the two diagonals
- * of a square matrix of integers
- * @n: 2D array
- * Return: nothing
+ * check - checks for the square root
+ * @a:int
+ * @b:int
+ *
+ * Return: int
  */
+int check(int a, int b)
+{
+if (a * a == b)
+return (a);
+if (a * a > b)
+return (-1);
+return (check(a + 1, b));
+}
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
+ */
 int _sqrt_recursion(int n)
 {
-if (n < 0) {
-return -1;
-} else if (n == 0 || n == 1) {
-return n;
-} else {
-return _sqrt_helper(n, 1, n);
+if (n == 0)
+return (0);
+return (check(1, n));
 }
-}
-
-int _sqrt_helper(int n, int start, int end)
-{
-if (end < start) {
-return -1;
-}
-
-int mid = (start + end) / 2;
-
-if (mid * mid == n) {
-return mid;
-} else if (mid * mid > n) {
-return _sqrt_helper(n, start, mid - 1);
-} else {
-return _sqrt_helper(n, mid + 1, end);
-}
-}
-
