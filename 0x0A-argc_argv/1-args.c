db8bcc32ba428a33cfdda9957c159ca25caeb494
Fad-fad <faduteimi@gmail.com>
2023-04-08 11:25:03 -0700

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- main
+ * @argc: jnj
+ * @argv: jkd
+ * Return: j 0 hf
+ */
+int main(int argc, char *argv[])
+{
+int i;
+printf("%d", argc);
+printf("\n");
+return (0);
+}
