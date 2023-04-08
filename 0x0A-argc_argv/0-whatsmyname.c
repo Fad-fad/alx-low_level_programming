9542c0fd4dc8ba1ccd3c6df718672084ed127b02
Fad-fad <faduteimi@gmail.com>
2023-04-08 11:22:56 -0700

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- function
+ * @argc: argument1
+ * @argv: argument2
+ * Return: jjnfjnjnfj 0 hfhfhf
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s", argv[i]);
+}
+printf("\n");
+return (0);
+}
