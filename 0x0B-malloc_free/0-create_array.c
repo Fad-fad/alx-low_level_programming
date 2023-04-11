b3828ba7f7f95650413a25bf9eb76be3c30e372a
Fad-fad <faduteimi@gmail.com>
2023-04-11 08:13:12 -0700

    task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strncpy- converts a string to an integer
+ * @dest: string to be converted
+ * @src: jfjffkkfk
+ * @n: intg
+ * Return: the int converted from the string
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *tab;
+unsigned int i;
+tab = malloc(size);
+for (i = 0; i < size; i++)
+{
+tab[i] = c;
+}
+return(tab);
+}
