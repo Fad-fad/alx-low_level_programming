eb38f7f46090282aa804c8fee8b816e6776a6397
Fad-fad <faduteimi@gmail.com>
2023-04-11 11:39:40 -0700

    task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *str_concat- converts a string to an integer
+ * @s1: string to be converted
+ * @s2: jfjffkkfk
+ * Return: the int converted from the string
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *tab;
+int i;
+int j;
+int k;
+int q;
+for (i = 0; s1[i] != '\0'; i++)
+{}
+for (j = 0; s2[j] != '\0'; j++)
+{}
+
+tab = malloc(i + j + 1);
+if (tab == NULL)
+return (tab);
+else
+{
+for (k = 0; k < i; k++)
+tab[k] = s1[i];
+q = 0;
+for (k = i - 1; k < i + j; k++)
+{
+tab[k] = s2[q];
+q++;
+}
+tab[i + j] = '\0';
+}
+return (tab);
+}
