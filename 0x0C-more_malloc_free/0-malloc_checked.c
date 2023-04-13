ab256ab4e3cd238ce2ccbca34b209b6c0667532e
Fad-fad <faduteimi@gmail.com>
2023-04-13 08:50:06 -0700

    task

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,17 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by alloc_grid
- * @grid: pointer to a 2 dimensional grid of integers
- * @height: height of the grid
- *
+ * *malloc_checked - frees a 2 dimension
+ * @b: pointer to a 2 dimensional grid of integers
  * Return: void
  */
 void *malloc_checked(unsigned int b)
 {
-    void *p = malloc(b);
-
-    if (p == NULL)
-    {
-        exit(98);
-    }
-
-    return (p);
+void *p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
 }
 
