4d47a6a0719622592dc1b69c785623e90935394f
Fad-fad <faduteimi@gmail.com>
2023-04-17 13:14:27 -0700

    task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+/**
+* init_dog - init
+* @d: pointer
+* @name: nam
+* @age: age
+* @owner: own
+*/
+#include <stdlib.h>
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+
+
