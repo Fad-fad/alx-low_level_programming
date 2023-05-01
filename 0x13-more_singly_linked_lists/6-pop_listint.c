7f121df9d20eee782c288d5d6d55dca723508991
Fad-fad <faduteimi@gmail.com>
2023-05-01 11:13:55 -0700

    its me again

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Write a function that deletes the head node
+ * @head: double pointer.
+ *
+ * Return: the head node's data (g)
+ */
+int pop_listint(listint_t **head)
+{
+listint_t reserve;
+int n;
+n = *head->n;
+reserve = *head->next;
+free(*head);
+*head = reserve;
+return (n);
+}
