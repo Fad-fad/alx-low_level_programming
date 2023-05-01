32024f02fcfcc28f2a65abf7b5a57799521c9208
Fad-fad <faduteimi@gmail.com>
2023-05-01 10:50:36 -0700

    its me again

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "lists.h"
-
 /**
  * free_listint2 - frees a ltint_t list
  * @head: pointer to  of the list
@@ -12,7 +11,7 @@ void free_listint2(listint_t **head)
 {
 listint_t *rempl, *helper;
 helper = *head;
-if (head == NULL)
+if (head == NULL || *head == NULL)
 return;
 while (helper != NULL)
 {
